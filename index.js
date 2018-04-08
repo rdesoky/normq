cbe4455e8a0a3d32b3c67374f741cd0149c490b9
Ramy (Home PC) <rdesoky@gmail.com>
2018-04-07 20:36:05 -0700

    Enhance normalization regex

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ if( args.length < 3 ){
 
 var out = args.length > 3 ? args[3] : 'text';
 var from = args.length > 4 ? parseInt(args[4]) : 0;
-var lines = args.length > 5 ? parseInt(args[5]) : 100;
+var lines = args.length > 5 ? parseInt(args[5]) : 0;
 
 
 var fileName = path.resolve(process.argv[2]);
@@ -36,33 +36,38 @@ var lineCounter = 0;
 lineReader.on('line', function(line){
     if(!colNames){
         colNames = line.split(",");
-    }else{
+    }
+    else{
         var record = {};
         line.split(",").forEach((val,i) => {
             record[colNames[i]]=val;
         });
+
+        // https://en.wikipedia.org/wiki/Arabic_script_in_Unicode
         //Remove Tashkeel
         record.plain=record.text.replace(
             /[\u0600-\u061f]|[\u064b-\u065e]|[\u0670]|[\u06d6-\u06ed]/g,
             ''
         );
+        
+        //Normalize similar characters
         record.norm = record.plain
             //Alef and Hamza
-            .replace(/[\u0622\u0623\u0625\ufe8d\ufe8e\ufe81\ufe82]/g ,"\u0627")
+            .replace(/[\u0622\u0623\u0625]|[\ufe81\ufe82\ufe8d\ufe8e]/g ,"\u0627")
             //Lam-alef
-            .replace(/[\u06d9\ufef5\ufef6\ufef7\ufef8\ufef9\ufefa\ufefb\ufefc]/g ,"\u0644\u0627")
+            .replace(/[\ufef5-\ufefc]/g ,"\u0644\u0627")
             //Waw
             .replace(/[\u0676\u0624]/g ,"\u0648")
             //Haa
             .replace(/[\u0629\ufe93\ufe94]/g,"\u0647")
             //Yaa
-            .replace(/[\u064a\ufef1\ufef2\ufef4\ufef3\u0626]/g ,"\u0649");
+            .replace(/[\u0626\u064a]|[\ufef1-\ufef4]/g ,"\u0649");
             //.replace(/ +/," ")//replace double space with one
 
-
-
         lineCounter++;
-        if(lineCounter>from&&lineCounter<(from+lines)){
+        
+        if( lineCounter>from
+            && ((lines==0)||(lineCounter<(from+lines))) ){
             console.log(record[out]);
         }
     }
